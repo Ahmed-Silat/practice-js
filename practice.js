1e4fccbd4dc7319260ff5bc1f464b13ac69050d8
Ahmed-Silat <ahmedsilat95@gmail.com>
2022-06-23 13:50:13 +0500

    time-program

--- practice.js
+++ practice.js
@@ -324,3 +324,22 @@ if (hour < 18) {
 else {
     greeting = "Good evening";
 }
+
+
+var time = Number(prompt("Enter time in hundred hours format"))
+
+if (time >= 0000 && time < 1200) {
+    alert("Good Morning!")
+}
+else if (time >= 1200 && time < 1700) {
+    alert("Good Afternoon!")
+}
+else if (time >= 1700 && time < 2100) {
+    alert("Good Evening!")
+}
+else if (time >= 2100 && time <= 2359) {
+    alert("Good Night!")
+}
+else {
+    alert("Invalid time input")
+}
